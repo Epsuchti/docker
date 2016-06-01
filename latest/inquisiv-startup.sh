84b8d2a9d03a59d9ec68b31f813f48d643b2a30b
Aaron Schmid <aaron.schmid@soxes.switzerland>
2016-06-01 11:00:02 +0200

    Changed startup

--- latest/inquisiv-startup.sh
+++ latest/inquisiv-startup.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-echo $1/web2py/
-echo $3:$4@$2
 
 # Checkout svn
 svn checkout $2 $1/web2py/ --username $3 --password $4 --non-interactive --trust-server-cert
 cp $1/web2py/parameters_8000.py $1/web2py/parameters_80.py
 
 # Start emperor
+echo Start uwsgi-emperor
 uwsgi --emperor /etc/uwsgi --logto /var/log/uwsgi.log
 
 # Startup nginx
+echo Start nginx
 nginx -g 'daemon off;'
 
