5c4b8e0cfc8b305567b0be5dcd0bc1a0ae3c3c2d
Aaron Schmid <aaron.schmid@soxes.switzerland>
2016-06-01 10:08:56 +0200

    Changed some things

--- latest/inquisiv-startup.sh
+++ latest/inquisiv-startup.sh
@@ -1,14 +1,14 @@
-
+#!/bin/sh
 echo $1/web2py/
 echo $3:$4@$2
 
 # Checkout svn
-svn checkout $2 $1/web2py/ --username $3 --password $4
-cp $1/web2py/parameters_8000.py $1/web2py/parameters_80.py
+#svn checkout $2 $1/web2py/ --username $3 --password $4
+#cp $1/web2py/parameters_8000.py $1/web2py/parameters_80.py
 
 # Start emperor
-uwsgi --emperor /etc/uwsgi --logto /var/log/uwsgi.log
+#uwsgi --emperor /etc/uwsgi --logto /var/log/uwsgi.log
 
 # Startup nginx
-nginx -g 'daemon off;'
+#nginx -g 'daemon off;'
 
