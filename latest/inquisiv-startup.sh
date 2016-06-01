4aaaeb12ad87f60508ec0c8b7a6efc246f4bb252
Aaron Schmid <aaron.schmid@soxes.switzerland>
2016-06-01 10:36:41 +0200

    Changed startup.sh

--- latest/inquisiv-startup.sh
+++ latest/inquisiv-startup.sh
@@ -3,7 +3,7 @@ echo $1/web2py/
 echo $3:$4@$2
 
 # Checkout svn
-svn checkout $2 $1/web2py/ --username $3 --password $4
+svn checkout $2 $1/web2py/ --username $3 --password $4 --non-interactive --trust-server-cert
 cp $1/web2py/parameters_8000.py $1/web2py/parameters_80.py
 
 # Start emperor
