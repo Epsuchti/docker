3002cf595058a1dc8b353445c31a46c301a40505
Aaron Schmid <aaron.schmid@soxes.switzerland>
2016-06-02 09:02:18 +0200

    Working on mysql support

--- latest/inquisiv-startup.sh
+++ latest/inquisiv-startup.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-echo Start inquisiv server...
+echo "Start inquisiv server..."
 
-echo Starting MySql...
-mysqld_safe --init-file=$1/mysql-init.sql &
+#echo "Starting MySql..."
+#mysqld_safe --init-file=$1/mysql-init.sql &
 
 # Checkout svn and import database
-if [! -f $1/web2py/applications/inquisiv/models/_config.py];
+if [ ! -f $1/web2py/applications/inquisiv/models/_config.py ]
 then
-	echo Checkout SVN
+	echo "Checkout SVN"
 	svn checkout $2 $1/web2py/ --username $3 --password $4 --non-interactive --trust-server-cert
 	cp $1/web2py/parameters_8000.py $1/web2py/parameters_80.py	
 fi
 
 # Start emperor
-echo Starting uwsgi-emperor...
+echo "Starting uwsgi-emperor..."
 uwsgi --emperor /etc/uwsgi --logto /var/log/uwsgi.log --uid www-data --gid www-data &
 
 # Startup nginx
-echo Starting nginx...
+echo "Starting nginx..."
 nginx -g 'daemon off;'
