a6826bdc5b14b103b329a113e299bbfcde7ed00b
chitter99 <as.sch@gmx.ch>
2016-06-03 08:12:04 +0200

    changed order

--- latest/inquisiv-startup.sh
+++ latest/inquisiv-startup.sh
@@ -9,6 +9,14 @@ service mysql start
 # Checkout svn and import database
 if [ ! -f /var/inquisiv/web2py/applications/inquisiv/models/_config.py ]
 then
+	# Checkout SVN
+	echo "Checkout SVN"
+	svn checkout $1 /var/inquisiv/web2py/ --username $2 --password $3 --non-interactive --trust-server-cert
+	
+	cp /var/inquisiv/web2py/parameters_8000.py /var/inquisiv/web2py/parameters_80.py
+	echo "Source code downloaded."
+	echo "You need to edit _config.py now"
+	
 	# Create DB
 	echo "Create Inquisiv DB"
 	mysql -uroot -e "CREATE DATABASE inquisiv;"
@@ -20,14 +28,6 @@ then
 	mysql -uroot < /var/inquisiv/web2py/database/page_data.sql
 	mysql -uroot < /var/inquisiv/web2py/database/grid_list.sql
 	mysql -uroot < /var/inquisiv/web2py/database/list_category_list_entry.sql
-	
-	# Checkout SVN
-	echo "Checkout SVN"
-	svn checkout $1 /var/inquisiv/web2py/ --username $2 --password $3 --non-interactive --trust-server-cert
-	
-	cp /var/inquisiv/web2py/parameters_8000.py /var/inquisiv/web2py/parameters_80.py
-	echo "Source code downloaded."
-	echo "You need to edit _config.py now"
 fi
 
 # Start emperor
