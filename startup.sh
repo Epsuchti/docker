9dd4f89f7c1030905bf4c6ccc400c3fcf1c53c37
Epsuchti <ez@it4user.ch>
2016-06-07 09:19:09 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-svn checkout $1 /var/www --username $2 --password $3 --non-interactive --trust-server-cert -q
-service mysql start
-service apache2 start
-vsftpd
+echo "Start inquisiv server..."
