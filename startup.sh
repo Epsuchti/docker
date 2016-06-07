3817d6b88621cf52a7d8e110824bccd5f4d8183b
Epsuchti <ez@it4user.ch>
2016-06-07 09:13:12 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+
 svn checkout $1 /var/www --username $2 --password $3 --non-interactive --trust-server-cert -q
 chmod -R 777 /var/www/*
 service mysql start
 service apache2 start
+vsftpd
